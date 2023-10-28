f15ba6002ab9d2f9757726ad069ece0954b519b9
andresam <andres@aamsis.co>
2023-10-28 12:54:56 -0500

    add texts to show errors or loading

--- src/suppliers/Suppliers.jsx
+++ src/suppliers/Suppliers.jsx
@@ -2,24 +2,42 @@ import React, { useEffect, useState } from "react";
 import "./Suppliers.scss"
 
 export const Suppliers = () => {
-  const [suppliers, setSuppliers] = useState();
+  const [suppliers, setSuppliers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
+    setIsLoading(true)
     fetch("https://gorest.co.in/public/v2/users")
       .then((data) => data.json())
       .then((suppliersList) => setSuppliers(suppliersList))
+      .catch(() => {
+        setError(true)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
   }, [])
-  
+
   return (
     <div className="suppliers">
-      <label htmlFor="supplier-dropdown" className="suppliers-list-label">
-        Supplier:
-        {' '}
-      {suppliers && <select className="suppliers-list" name="supplier-dropdown">
-        {suppliers.map((supplier) => <option key={`supplier-${supplier.id}`} className="suppliers-list-item">
-          {supplier.name}
-        </option>)}
-      </select>}
-      </label>
+      {error ?
+        <div className="suppliers-error-container">
+          <p>Error: the suppliers list could not be loaded</p>
+        </div>
+        : isLoading
+          ? <p>Loading...</p>
+          : (
+            <label htmlFor="supplier-dropdown" className="suppliers-list-label">
+              Supplier: {' '}
+              <select className="suppliers-list" name="supplier-dropdown">
+                {suppliers.map((supplier) => (
+                  <option key={`supplier-${supplier.id}`} className="suppliers-list-item">
+                    {supplier.name}
+                  </option>
+                ))}
+              </select>
+            </label>
+          )}
     </div>
   )
 }
