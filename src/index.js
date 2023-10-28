1c7085c99553cbb76b86dff3292118895e41db6a
andresam <andres@aamsis.co>
2023-10-28 18:55:04 -0500

    add font to the app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CartProvider } from './Cart/CartContext';
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
